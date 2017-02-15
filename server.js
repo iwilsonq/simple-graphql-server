871700dd61a1983f0d4d31e106522c5d5ee05438
iwilsonq <iwilsonq@gmail.com>
2017-02-15 15:31:06 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
+
+app.listen(PORT, console.log(`listening on http://localhost:${PORT}`));
